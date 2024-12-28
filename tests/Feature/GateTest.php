86963dd89860e2a2131cbb155a2d37e64a2dda78
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-28 21:59:14 +0700

    Gate Response

--- tests/Feature/GateTest.php
+++ tests/Feature/GateTest.php
@@ -28,4 +28,47 @@ class GateTest extends TestCase
         self::assertTrue(Gate::allows("delete-contact", $contact));
 
     }
+
+    public function testGateMethod()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $user = User::where("email", "test@gmail.com")->firstOrFail();
+        Auth::login($user);
+
+        $contact = Contact::where("email", "contact@gmail.com")->firstOrFail();
+
+        self::assertTrue(Gate::allows("get-contact", $contact));
+        self::assertTrue(Gate::allows("update-contact", $contact));
+        self::assertTrue(Gate::allows("delete-contact", $contact));
+
+        self::assertTrue(Gate::any(["get-contact", "update-contact", "delete-contact"], $contact));
+        self::assertFalse(Gate::none(["get-contact", "update-contact", "delete-contact"], $contact));
+    }
+
+    public function testGateNonLogin()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $user = User::where("email", "test@gmail.com")->firstOrFail();
+        $gate = Gate::forUser($user);
+
+        $contact = Contact::where("email", "contact@gmail.com")->firstOrFail();
+
+        self::assertTrue($gate->allows("get-contact", $contact));
+        self::assertTrue($gate->allows("update-contact", $contact));
+        self::assertTrue($gate->allows("delete-contact", $contact));
+    }
+
+    public function testGateResponse()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $user = User::where("email", "test@gmail.com")->firstOrFail();
+        Auth::login($user);
+
+        $response = Gate::inspect("create-contact");
+        self::assertFalse($response->allowed());
+        self::assertEquals("You are not admin", $response->message());
+    }
 }
