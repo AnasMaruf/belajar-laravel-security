c5ebe5982289b13a8b36fdc6ce464ffa4efb2041
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-30 07:12:58 +0700

    Authorizable

--- database/migrations/2024_12_29_231316_create_todos_table.php
+++ database/migrations/2024_12_29_231316_create_todos_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             $table->unsignedBigInteger("user_id")->nullable(false);
             $table->softDeletes();
             $table->timestamps();
-            $table->foreign("user_id")->references("id")->on("users");
+            $table->foreign("user_id")->references("id")->on("users")          ->onDelete("cascade")
+            ->onUpdate("cascade");
         });
     }
 
